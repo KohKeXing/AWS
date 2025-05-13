d6f1794a7d1ef4bbec548888fa7717ccc04a3a17
KohKeXing <kohkx-pm23@student.tarc.edu.my>
2025-05-13 23:44:53 +0800

    Update admin_order_detail.php

--- CloudAssingment/admin_order_detail.php
+++ CloudAssingment/admin_order_detail.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 // Include database connection
 if (file_exists('db.php')) {
     include 'db.php';

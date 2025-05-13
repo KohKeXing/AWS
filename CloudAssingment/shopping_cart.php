1977e1927ede6ba6b2d7576ff225b9bb5301422b
KohKeXing <kohkx-pm23@student.tarc.edu.my>
2025-05-14 00:01:47 +0800

    Update shopping_cart.php

--- CloudAssingment/shopping_cart.php
+++ CloudAssingment/shopping_cart.php
@@ -1,5 +1,8 @@
 <?php
     session_start(); // Start the session at the beginning
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
     
     // Check if user is logged in
     if (!isset($_SESSION['customerid'])) {

3f6bfa3a5b4cec1552ddbed5dad709029d64f1a9
KohKeXing <kohkx-pm23@student.tarc.edu.my>
2025-05-14 21:17:23 +0800

    Update editCustomer.php

--- CloudAssingment/editCustomer.php
+++ CloudAssingment/editCustomer.php
@@ -3,21 +3,7 @@ session_start();
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
-// Database connection
-$host = 'localhost';
-$dbname = 'graduation_store';
-$username = 'root';
-$password = '';
-
-// Create connection
-$conn = new mysqli($host, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
+include 'help.php';
 
 $customerid = $_SESSION['customerid'] ?? null;
 if (!$customerid) {

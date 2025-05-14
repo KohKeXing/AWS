ef990d4ea4854483f1716298aebf3040cce4ac4f
KohKeXing <kohkx-pm23@student.tarc.edu.my>
2025-05-14 21:24:11 +0800

    Update index.php

--- CloudAssingment/index.php
+++ CloudAssingment/index.php
@@ -1,9 +1,9 @@
 <?php
+include 'help.php';
+session_start();
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-include 'help.php';
-session_start();
 
 // After successful login, add this code:
 if (isset($_SESSION['customerid'])) {

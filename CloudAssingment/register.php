5a3e60c31d3aad3ce43523ba9f9ad44d4304e7c3
KohKeXing <kohkx-pm23@student.tarc.edu.my>
2025-05-14 21:31:09 +0800

    Update register.php

--- CloudAssingment/register.php
+++ CloudAssingment/register.php
@@ -1,22 +1,7 @@
 <?php
 // Removed session_start();
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
-// Database connection
-$host = 'localhost';
-$dbname = 'graduation_store';
-$username = 'root';
-$password = '';
-
-// Create connection
-$conn = new mysqli($host, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+include 'help.php';
 
 // Generate next customer ID
 $sql = "SELECT customerid FROM customer ORDER BY customerid DESC LIMIT 1";

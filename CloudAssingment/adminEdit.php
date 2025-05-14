0fdea792e2a6654e1f96336864028af6a01808d8
KohKeXing <kohkx-pm23@student.tarc.edu.my>
2025-05-14 21:01:37 +0800

    Update adminEdit.php

--- CloudAssingment/adminEdit.php
+++ CloudAssingment/adminEdit.php
@@ -1,23 +1,9 @@
 <?php
 // adminEdit.php
+include 'help.php';
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
-// Database connection
-$host = 'localhost';
-$dbname = 'graduation_store';
-$username = 'root';
-$password = '';
-
-// Create connection
-$conn = new mysqli($host, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
 $managerID = $_GET['managerID'] ?? null;
 $managerID = $conn->real_escape_string($managerID);
 

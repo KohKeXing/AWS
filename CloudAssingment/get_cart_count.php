ffd9e896166c5bab19af290c95c4dc5289c68ae4
KohKeXing <kohkx-pm23@student.tarc.edu.my>
2025-05-14 21:18:52 +0800

    Update get_cart_count.php

--- CloudAssingment/get_cart_count.php
+++ CloudAssingment/get_cart_count.php
@@ -1,18 +1,11 @@
 <?php
 session_start();
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 if (!isset($_SESSION['customerid'])) {
     echo json_encode(['count' => 0]);
     exit();
 }
 
-$host = 'localhost';
-$dbname = 'graduation_store';
-$username = 'root';
-$password = '';
-
+include 'help.php';
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
